c7408a5f9d881e7043862a9f1f577a5c6936c9b1
Ivan Calderon <iwano@hotmail.com>
2013-05-03 12:53:11 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 OmniauthApp::Application.routes.draw do
+  get 'auth/google_oauth2/callback' => 'sessions#create' do
+    content_type 'text/plain'
+    request.env['omniauth.auth'].to_hash.inspect rescue "No Data"
+  end
+  root to: 'home#index'
+  
+  get '/auth/failure' do
+    content_type 'text/plain'
+    request.env['omniauth.auth'].to_hash.inspect rescue "No Data"
+  end
 
+  delete '/sessions/destroy' => 'sessions#destroy', as: :log_out
 end
